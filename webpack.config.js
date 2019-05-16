376088359b3ce1a79f79a6923d2e810f409504a1
Lucas Lee <freedom@Lucass-MacBook.fios-router.home>
2019-05-16 23:55:32 +0800

    Added html-webpack-plugin to copy index.html over and minify

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,31 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  entry: "./src/index.component.jsx",
-  output: {
+    entry: "./src/index.component.jsx",
+    output: {
       filename: "bundle.js",
       path: __dirname + "/build"
-  },
-  module: {
-      rules: [
-          { 
-            exclude: /node_modules/,
-            loader: "babel-loader",
-            test: /\.(js|jsx)$/,  
-          },
-      ]
-  },
-};
+    },
+
+    module: {
+      rules: [{
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        test: /\.(js|jsx)$/,
+      }, ]
+    },
+
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./public/index.html",
+        filename: "./index.html",
+        minify: {
+          collapseWhitespace: true,
+          removeComments: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          useShortDoctype: true
+        }
+      })]
+    };
