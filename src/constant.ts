a9516b558ecf36a8c3cee5124dae0c48f3aa6be3
Lucas Lee <lucas@lucaslee.net>
2019-05-20 15:33:37 +0800

    Added favicon - Created a new api key

--- src/constant.ts
+++ src/constant.ts
@@ -1,7 +1,7 @@
 // Number of images per api call
 export const PAGE_SIZE: number = 50;
 export const INPUT_TIMEOUT: number = 2000;
-export const FLICKR_API_KEY = "0ff83264ffe9018839a4a4d171be6db6";
+export const FLICKR_API_KEY = "b7ebd0b4890f3d34772a273710403a7b";
 export const FLICKR_URL = "/services/rest/";
 export const SLOGAN = "{{rawrrrr!}} for images";
 export const APP_NAME = "flick-rawr!";
